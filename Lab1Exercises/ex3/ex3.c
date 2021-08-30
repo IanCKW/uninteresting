7b66c3c93b3bff1131a51a78aef5c75f0c066fe8
ianckw <iankwchan@gmail.com>
2021-08-30 18:56:42 +0800

    ex3 complete

--- Lab1Exercises/ex3/ex3.c
+++ Lab1Exercises/ex3/ex3.c
@@ -1,10 +1,10 @@
 /*************************************
 * Lab 1 Exercise 3
-* Name:
-* Student No:
-* Lab Group:
+* Name: ian Chan
+* Student No: A0197089J
+* Lab Group: 11
 *************************************/
-
+#include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -25,20 +25,89 @@
 #define REVERSE_LIST 4
 #define RESET_LIST 5
 #define MAP 6
+#define PRINT_LIST 7
 
-int main(int argc, char **argv) {
+void print_list(list *lst);
+void update_functions();
+void run_instruction(list *lst, int instr, char line[]);
+
+int main(int argc, char **argv) { // double pointer as it's a nested array
     if (argc != 2) {
         fprintf(stderr, "Error: expecting 1 argument, %d found\n", argc - 1);
         exit(1);
     }
+    // HANDLE EMPY FILES
 
     // We read in the file name provided as argument
     char *fname = argv[1];
 
-    // Update the array of function pointers
-    // DO NOT REMOVE THIS CALL
-    // (You may leave the function empty if you do not need it)
-    update_functions();
+    //update_functions();
 
     // Rest of code logic here
+    FILE *fPointer;
+    fPointer = fopen(fname,"r"); // returns a pointer to the file descriptor. "r" means read only
+    
+    list *lst = (list *)malloc(sizeof(list));
+    lst->head = NULL;
+
+    char line[255];
+    int instr;
+    while(!feof(fPointer)){ //while the file pointer is not at the end of the file
+        fgets(line,255,fPointer);
+        sscanf(line,"%d",&instr);
+        run_instruction(lst, instr, line);
+    }
+    
+    reset_list(lst);
+    free(lst);
+    fclose(fname);
+}
+
+void run_instruction(list *lst, int instr, char line[]) {
+    int instruction, index, data, offset, map_func;
+    switch (instr) {
+        case PRINT_LIST:
+            print_list(lst);
+            break;
+        case INSERT_AT:
+            sscanf(line, "%d %d %d", &instruction, &index, &data);
+            insert_node_at(lst, index, data);
+            break;
+        case DELETE_AT:
+            sscanf(line, "%d %d", &instruction, &index);
+            delete_node_at(lst, index);
+            break;
+        case ROTATE_LIST:
+            sscanf(line, "%d %d", &instruction, &offset);
+            rotate_list(lst, offset);
+            break;
+        case REVERSE_LIST:
+            reverse_list(lst);
+            break;
+        case RESET_LIST:
+            reset_list(lst);
+            break;
+        case SUM_LIST:
+            printf("%d\n",sum_list(lst));
+            break;
+        case MAP:
+            sscanf(line, "%d %d", &instruction, &map_func);
+            map(lst, func_list[map_func]);
+    }
+}
+
+// Prints out the whole list in a single line
+void print_list(list *lst) {
+    if (lst->head == NULL) {
+        printf("[ ]\n");
+        return;
+    }
+
+    printf("[ ");
+    node *curr = lst->head;
+    do {
+        printf("%d ", curr->data);
+        curr = curr->next;
+    } while (curr != lst->head);
+    printf("]\n");
 }
--- Lab1Exercises/ex3/node.c
+++ Lab1Exercises/ex3/node.c
@@ -12,42 +12,197 @@
 #include <stdlib.h>
 #include <string.h>
 
+int get_size(list *lst);
+node* get_tail(list *lst, int size_of_list);
+node* get_previous(list* lst, int index);
+node* get_next(list* lst, int index);
+
 // Copy in your implementation of the functions from ex2.
 // There is one extra function called map which you have to fill up too.
 // Feel free to add any new functions as you deem fit.
 
-// Inserts a new node with data value at index (counting from head
-// starting at 0).
+// Inserts a new node with data value at index (counting from head starting at 0).
 // Note: index is guaranteed to be valid.
-void insert_node_at(list *lst, int index, int data) {
-}
 
+
+void insert_node_at(list *lst, int index, int data) {
+    
+    node *replacement = malloc(4);
+    (*replacement).next = NULL;
+    (*replacement).data = data;
+    int size_of_list = get_size(lst);
+    
+    if (index < size_of_list){
+        node *next_node = get_next(lst, index);
+        replacement->next = next_node;
+        //printf("issue is not here index < size");
+    }
+    if (index == 0){
+        lst->head = replacement;
+    }
+    if (index > 0){
+        node *previous_node = get_previous(lst, index);
+        previous_node->next = replacement;
+        //printf("issue is not here index > 0");
+    }
+    size_of_list++; // the list has grown by 1
+    node *tail_node_pointer = get_tail(lst, size_of_list);
+    tail_node_pointer->next = lst->head;
+
+}
+//helpful commands: fopen, fscanf, fclose. Can print to stderr using fprintf is the file does not exit
 // Deletes node at index (counting from head starting from 0).
 // Note: index is guarenteed to be valid.
 void delete_node_at(list *lst, int index) {
+    if (lst->head == NULL){
+        return;
+    }
+    node* deleted = get_next(lst, index);
+    int size_of_list = get_size(lst);
+    if(size_of_list == 1){
+        lst->head = NULL;
+        free(deleted->next);
+        free(deleted);
+        return;
+    }
+    else if (index == 0){
+        lst->head = deleted->next;
+    }
+    else { // index > 0
+        node *prev = get_previous(lst, index);
+        prev->next = deleted->next; // if index is at the end of the list, prev-> will change to lst.head
+    }
+    free(deleted);
+    free(deleted);
+    size_of_list--; // the list has shrunk by 1
+    node *tail_node_pointer = get_tail(lst, size_of_list);
+    tail_node_pointer->next = lst->head;
 }
 
 // Rotates list by the given offset.
 // Note: offset is guarenteed to be non-negative.
 void rotate_list(list *lst, int offset) {
+    int size_of_list = get_size(lst);
+    int offset_mod = offset % size_of_list;
+    node* new_head = get_next(lst, offset_mod);
+    lst->head = new_head;
 }
 
 // Reverses the list, with the original "tail" node
 // becoming the new head node.
 void reverse_list(list *lst) {
+    int size_of_list = get_size(lst);
+    if (lst->head == NULL | size_of_list == 1) {
+        return;
+    }
+    node* curr = lst->head;
+    node* next = curr->next;
+    node* prev = NULL;
+    for (int i=0;i<size_of_list;i++){
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+        next = next->next;
+    }
+    lst->head = prev;
+    curr->next = lst->head;
 }
 
 // Resets list to an empty state (no nodes) and frees
 // any allocated memory in the process
 void reset_list(list *lst) {
+    if (lst->head == NULL){
+        return;
+    }
+    int list_size = get_size(lst);
+    if (list_size==1){
+        node* curr = lst->head;
+        free(curr->next);
+        free(curr);
+        lst->head = NULL;
+        return;
+    }
+    node* curr = lst->head;
+    node * next = curr->next;
+    for (int i=0; i<list_size-2;i++){
+        free(curr->next);
+        curr = next;
+        next = next->next;
+    }
+    free(next->next);
+    free(next);
+    free(curr->next);
+    free(curr);
+    lst->head = NULL;
+}
+
+int get_size(list *lst){
+    if (lst->head == NULL){
+        return 0;
+    }
+    node* current = lst->head;
+    if (current == NULL){
+        return 0;
+    }
+    int size = 1;
+    while (current->next != lst->head){ // it points to the front
+        size += 1;
+        current = current->next;
+    }
+    return size;
+}
+
+node* get_tail(list *lst, int size_of_list){
+    node* tail = lst->head;
+    for(int i = 0; i < size_of_list - 1; i++){
+        tail = tail->next;
+    }
+    return tail;
+}
+
+node* get_previous(list *lst, int index){
+    node* current = lst->head;
+    for(int i = 0; i < index-1; i++){
+        current = current->next;
+    }
+    return current;
+}
+
+node* get_next(list *lst, int index){
+    node* current = lst->head;
+    for(int i = 0; i < index; i++){
+        current = current->next;
+    }
+    return current;
 }
 
 // Traverses list and applies func on data values of
 // all elements in the list.
 void map(list *lst, int (*func)(int)) {
+    if (lst->head == NULL){
+        return;
+    }
+    int list_size = get_size(lst);
+    node* curr = lst->head;
+    for(int i=0; i<list_size; i++){
+        int num = curr->data;
+        curr->data = (*func)(num);
+        curr = curr->next;
+    }
 }
 
 // Traverses list and returns the sum of the data values
 // of every node in the list.
 long sum_list(list *lst) {
+    int sum = 0;
+    int list_size = get_size(lst);
+    if (lst->head == NULL){
+        return 0;
+    }
+    node* curr = lst->head;
+    for(int i=0; i<list_size; i++){
+        sum += curr->data;
+        curr = curr->next;
+    }
+    return sum;
 }
